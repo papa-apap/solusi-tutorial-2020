7d6b5864383719da5f819e410182081abd07bcd7
Prissy Azzahra <prissy.azzahra@gmail.com>
2020-11-19 12:18:25 +0700

    fix: fix hospital url typo

--- haidokter/src/main/java/apap/tutorial/haidokter/rest/Setting.java
+++ haidokter/src/main/java/apap/tutorial/haidokter/rest/Setting.java
@@ -4,5 +4,5 @@ public class Setting {
     final public static String resepUrl = "https://60dd6513-9bb1-4a3b-beed-150be462f07a.mock.pstmn.io";
 
     /** LATIHAN **/
-    final public static String hospitalUrl = "hhttp://www.communitybenefitinsight.org/api/get_hospitals.php";
+    final public static String hospitalUrl = "http://www.communitybenefitinsight.org/api/get_hospitals.php";
 }
